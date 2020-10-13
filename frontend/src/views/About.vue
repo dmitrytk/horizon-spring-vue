83dfaaa049cc5b55b5e1cc27f1bdf9defc4a0c28
Dmitry T <takkand.d@gmail.com>
2020-10-13 12:50:55 +0500

    Initial

---
+++ frontend/src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <input v-model="content" type="text">
+    <button @click="getContent">Load</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'About',
+  data() {
+    return {
+      content: '',
+    };
+  },
+  methods: {
+    getContent() {
+      axios.get('/api')
+        .then((result) => {
+          this.content = result.data;
+        });
+    },
+  },
+};
+
+</script>
